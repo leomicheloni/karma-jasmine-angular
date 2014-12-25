91525a1650e047d6987e99ad5517084f52e3ee55
Leonardo <=>
2014-12-25 18:40:48 -0300

    simple controller unit testing

--- test/fixture.js
+++ test/fixture.js
@@ -1,16 +1,24 @@
 describe("calculator test", function () {
 	var calculator;
+	var controller;
 
 	beforeEach(module("myapp"));
 
-	beforeEach(inject(function ($injector) {
+	beforeEach(inject(function ($rootScope, $controller, $injector) {
 			calculator = $injector.get("calculatorService");
+			controller = $controller("mainController", {
+					$scope : $rootScope
+				});
 		}));
 
 	it("simple add should add", function () {
 		var result = calculator.add(3, 5);
 		expect(result).toBe(8);
 
+	});
+
+	it("controller should exist", function () {
+		expect(controller).not.toBe(undefined);
 	});
 
 });
