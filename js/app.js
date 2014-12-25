6380c530cb6d2bc7aa097ffafb597eddceeb9df4
Leonardo <=>
2014-12-25 18:34:04 -0300

    simple adding function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 var app = angular.module("myapp", []);
 
-app.factory("calculatorService", function(){
+app.factory("calculatorService", function () {
+	return {
+		add : function (a, b) {
+			return a + b;
+		}
+	};
+
 });
-	
-app.controller("mainController", function($scope){
-});
+
+app.controller("mainController", function ($scope) {});
--- test/fixture.js
+++ test/fixture.js
@@ -7,4 +7,10 @@ describe("calculator test", function () {
 			calculator = $injector.get("calculatorService");
 		}));
 
+	it("simple add should add", function () {
+		var result = calculator.add(3, 5);
+		expect(result).toBe(8);
+
+	});
+
 });
